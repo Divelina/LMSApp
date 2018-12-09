e44c5f340b589f7f26dc4fff95f56d9b91224cc0
Divelina <divelina@phys.uni-sofia.bg>
2018-12-09 14:27:31 +0200

    Account manage navigation fixed with the initial layout.

--- src/Web/LMSApp/wwwroot/js/site.js
+++ src/Web/LMSApp/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//$('#acc-manage-nav a').on('click', function () {
+//    $('#acc-manage-nav li').find('a.act').removeClass('act');
+//    $(this).addClass('act');
+//});
